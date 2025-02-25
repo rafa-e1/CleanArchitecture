e0f20a0d2fe6e147da1d6384367883463f93bcb2
RAFA <rafa71802937@gmail.com>
2025-02-25 18:35:54 +0900

    feat: 오류 처리 및 사용자 목록을 위한 도메인 엔티티 구현

---
+++ CleanArchitecture/Sources/Domain/Entity/CoreDataError.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataError.swift
+//  CleanArchitecture
+//
+//  Created by RAFA on 2/25/25.
+//
+
+import Foundation
+
+public enum CoreDataError: Error {
+    case entityNotFound(String)
+    case saveError(String)
+    case readError(String)
+    case deleteError(String)
+
+    public var description: String {
+        switch self {
+        case .entityNotFound(let objectName): "객체를 찾을 수 없습니다. - \(objectName)"
+        case .saveError(let message):         "객체 저장 에러 - \(message)"
+        case .readError(let message):         "객체 조회 에러 - \(message)"
+        case .deleteError(let message):       "객체 삭제 에러 - \(message)"
+        }
+    }
+}
---
+++ CleanArchitecture/Sources/Domain/Entity/NetworkError.swift
@@ -0,0 +1,28 @@
+//
+//  NetworkError.swift
+//  CleanArchitecture
+//
+//  Created by RAFA on 2/25/25.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case urlError
+    case invalid
+    case decodingError(String)
+    case noData
+    case serverError(Int)
+    case requestFailed(String)
+
+    public var description: String {
+        switch self {
+        case .urlError:                       "URL이 올바르지 않습니다."
+        case .invalid:                        "응답값이 유효하지 안습니다."
+        case .decodingError(let description): "디코딩 에러 - \(description)"
+        case .noData:                         "데이터가 없습니다."
+        case .serverError(let statusCode):    "서버 에러 - \(statusCode)"
+        case .requestFailed(let message):     "서버 요청 실패 - \(message)"
+        }
+    }
+}
