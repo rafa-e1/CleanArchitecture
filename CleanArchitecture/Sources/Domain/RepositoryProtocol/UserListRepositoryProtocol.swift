a7b083626552e817bb63dfeedb0d32a02ae7f8f5
RAFA <rafa71802937@gmail.com>
2025-02-26 01:15:16 +0900

    feat: UseCase 구현

---
+++ CleanArchitecture/Sources/Domain/RepositoryProtocol/UserListRepositoryProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  UserListRepositoryProtocol.swift
+//  CleanArchitecture
+//
+//  Created by RAFA on 2/26/25.
+//
+
+import Foundation
+
+protocol UserListRepositoryProtocol {
+    func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError>
+    func getFavoriteUsers() -> Result<[UserListItem], CoreDataError>
+    func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError>
+    func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError>
+}
---
+++ CleanArchitecture/Sources/Domain/UseCase/UserListUseCase.swift
@@ -0,0 +1,40 @@
+//
+//  UserListUseCase.swift
+//  CleanArchitecture
+//
+//  Created by RAFA on 2/26/25.
+//
+
+import Foundation
+
+protocol UserListUseCaseProtocol {
+    func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError>
+    func getFavoriteUsers() -> Result<[UserListItem], CoreDataError>
+    func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError>
+    func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError>
+}
+
+struct UserListUseCase: UserListUseCaseProtocol {
+
+    private let repository: UserListRepositoryProtocol
+
+    init(repository: UserListRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError> {
+        await repository.fetchUser(query: query, page: page)
+    }
+    
+    func getFavoriteUsers() -> Result<[UserListItem], CoreDataError> {
+        repository.getFavoriteUsers()
+    }
+    
+    func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError> {
+        repository.saveFavoriteUser(user: user)
+    }
+    
+    func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError> {
+        repository.deleteFavoriteUser(userID: userID)
+    }
+}
