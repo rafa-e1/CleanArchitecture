ecd63e9c4a171b8a4362fd2fe10e649b1acd6cca
RAFA <rafa71802937@gmail.com>
2025-02-26 16:19:28 +0900

    refactor: Rename `invalid` case to `invalidResponse`

--- CleanArchitecture/Sources/Domain/Entity/NetworkError.swift
+++ CleanArchitecture/Sources/Domain/Entity/NetworkError.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public enum NetworkError: Error {
     case urlError
-    case invalid
+    case invalidResponse
     case decodingError(String)
     case noData
     case serverError(Int)
@@ -18,7 +18,7 @@ public enum NetworkError: Error {
     public var description: String {
         switch self {
         case .urlError:                       "URL이 올바르지 않습니다."
-        case .invalid:                        "응답값이 유효하지 안습니다."
+        case .invalidResponse:                        "응답값이 유효하지 안습니다."
         case .decodingError(let description): "디코딩 에러 - \(description)"
         case .noData:                         "데이터가 없습니다."
         case .serverError(let statusCode):    "서버 에러 - \(statusCode)"
