2fa441c6bc977dddd009d5a613e2a6710b864352
RAFA <rafa71802937@gmail.com>
2025-02-26 16:54:34 +0900

    feat: Add UserNetwork class for fetching user data

---
+++ CleanArchitecture/Sources/Data/Network/UserNetwork.swift
@@ -0,0 +1,25 @@
+//
+//  UserNetwork.swift
+//  CleanArchitecture
+//
+//  Created by RAFA on 2/26/25.
+//
+
+import Foundation
+
+final class UserNetwork {
+
+    private let manager: NetworkManagerProtocol
+
+    init(manager: NetworkManagerProtocol) {
+        self.manager = manager
+    }
+
+    func fetchUsers(
+        query: String,
+        page: Int
+    ) async -> Result<UserListResult, NetworkError> {
+        let url = "https://api.github.com/search/users?q=\(query)&page=\(page)"
+        return await manager.fetchData(url: url, method: .get, parameters: nil)
+    }
+}
