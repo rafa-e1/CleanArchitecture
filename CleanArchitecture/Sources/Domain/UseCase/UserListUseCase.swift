1a98c6fba36ca4b6f3bab6fcdbceddfe6984fee0
RAFA <rafa71802937@gmail.com>
2025-02-28 13:49:09 +0900

    feat: Add methods to check favorite state and convert list

--- CleanArchitecture/Sources/Domain/UseCase/UserListUseCase.swift
+++ CleanArchitecture/Sources/Domain/UseCase/UserListUseCase.swift
@@ -12,6 +12,12 @@ protocol UserListUseCaseProtocol {
     func getFavoriteUsers() -> Result<[UserListItem], CoreDataError>
     func saveFavoriteUser(user: UserListItem) -> Result<Bool, CoreDataError>
     func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError>
+    func checkFavoriteState(
+        fetchUsers: [UserListItem],
+        favoriteUsers: [UserListItem]
+    ) -> [(user: UserListItem, isFavorite: Bool)]
+
+    func convertListToDictionary(favoriteUsers: [UserListItem]) -> [String: [UserListItem]]
 }
 
 struct UserListUseCase: UserListUseCaseProtocol {
@@ -36,5 +42,28 @@ struct UserListUseCase: UserListUseCaseProtocol {
     
     func deleteFavoriteUser(userID: Int) -> Result<Bool, CoreDataError> {
         repository.deleteFavoriteUser(userID: userID)
+    }
+
+    func checkFavoriteState(
+        fetchUsers: [UserListItem],
+        favoriteUsers: [UserListItem]
+    ) -> [(user: UserListItem, isFavorite: Bool)] {
+        let favoriteSet = Set(favoriteUsers)
+        return fetchUsers.map { user in
+            if favoriteSet.contains(user) {
+                return (user: user, isFavorite: true)
+            } else {
+                return (user: user, isFavorite: false)
+            }
+        }
+    }
+
+    func convertListToDictionary(favoriteUsers: [UserListItem]) -> [String : [UserListItem]] {
+        favoriteUsers.reduce(into: [String: [UserListItem]]()) { dict, user in
+            if let firstString = user.login.first {
+                let key = String(firstString).uppercased()
+                dict[key, default: []].append(user)
+            }
+        }
     }
 }
