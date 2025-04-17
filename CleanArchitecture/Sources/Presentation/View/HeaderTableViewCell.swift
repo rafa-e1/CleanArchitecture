2973533f649191eb82bd5069d1a837c5fea83cc8
RAFA <rafa71802937@gmail.com>
2025-04-17 18:03:04 +0900

    feat: HeaderTableViewCell 구현
    HeaderTableViewCell.swift 파일을 프로젝트에 추가함
    
    - UITableViewCell을 상속받는 HeaderTableViewCell 클래스 구현
    - titleLabel을 사용하여 셀의 제목 표시
    - apply 메서드를 통해 UserListCellData를 적용할 수 있도록 함

---
+++ CleanArchitecture/Sources/Presentation/View/HeaderTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  HeaderTableViewCell.swift
+//  CleanArchitecture
+//
+//  Created by RAFA on 4/17/25.
+//
+
+import UIKit
+
+final class HeaderTableViewCell: UITableViewCell {
+
+    static let id = "HeaderTableViewCell"
+
+    private let titleLabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(titleLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.bottom.equalToSuperview().inset(10)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func apply(cellData: UserListCellData) {
+        guard case let .header(title) = cellData else { return }
+
+        titleLabel.text = title
+    }
+}
